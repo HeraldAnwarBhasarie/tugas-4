0766e33cae2a39b3e5d696ee345515c5066f8089
HeraldAnwarBhasarie <dhe_baz@ymail.com>
2016-03-27 22:32:40 +0700

    mas ini udh laporanku

---
+++ src/src/data.java
@@ -0,0 +1,79 @@
+package bab4;
+
+public class data {
+
+    public String Nama;
+    protected String NamaLengkap;
+    public String Alamat;
+    protected String AlamatLengkap;
+    public String NoTlpn;
+    private String NoTlpn1;
+    protected String Email;
+    private String Email1;
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String Nama) {
+        this.Nama = Nama;
+    }
+
+    public String getNamaLengkap() {
+        return NamaLengkap;
+    }
+
+    public void setNamaLengkap(String NamaLengkap) {
+        this.NamaLengkap = NamaLengkap;
+    }
+
+    public String getAlamat() {
+        return Alamat;
+    }
+
+    public void setAlamat(String Alamat) {
+        this.Alamat = Alamat;
+    }
+
+    public String getAlamatLengkap() {
+        return AlamatLengkap;
+    }
+
+    public void setAlamatLengkap(String AlamatLengkap) {
+        this.AlamatLengkap = AlamatLengkap;
+    }
+
+    public String getNoTlpn() {
+        return NoTlpn;
+    }
+
+    public void setNoTlpn(String NoTlpn) {
+        this.NoTlpn = NoTlpn;
+    }
+
+    public String getNoTlpn1() {
+        return NoTlpn1;
+    }
+
+    public void setNoTlpn1(String NoTlpn1) {
+        this.NoTlpn1 = NoTlpn1;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getEmail1() {
+        return Email1;
+    }
+
+    public void setEmail1(String Email1) {
+        this.Email1 = Email1;
+    }
+    
+     
+}
